8b2048af74dd6652932735bc1ab737db154bd13d
Judiza Rodriguez <jurodriguez.88@gmail.com>
2025-07-05 08:11:29 -0400

    REFACTOR- Se extrae lógica de validación de estudiante a método privado

--- src/main/java/com/alkemy/tdd/studentmanagement/service/EstudianteService.java
+++ src/main/java/com/alkemy/tdd/studentmanagement/service/EstudianteService.java
@@ -7,16 +7,21 @@ public class EstudianteService {
     private static Long nextId = 1L;
 
     public Estudiante crearEstudiante(Estudiante estudiante) {
+
+        validateEstudiante(estudiante);
+
+        estudiante.setId(nextId);
+        nextId++;
+
+        return estudiante;
+    }
+
+    private void validateEstudiante(Estudiante estudiante) {
         if (estudiante == null) {
             throw new IllegalArgumentException("El estudiante no puede ser nulo.");
         }
         if (estudiante.getNombre() == null || estudiante.getNombre().trim().isEmpty()) {
             throw new IllegalArgumentException("El nombre del estudiante es requerido.");
         }
-
-        estudiante.setId(nextId);
-        nextId++;
-
-        return estudiante;
     }
 }

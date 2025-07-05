8ccf99aa2eeadb5042fc8fb48546eb1db3cf5a56
Judiza Rodriguez <jurodriguez.88@gmail.com>
2025-07-05 07:56:57 -0400

    GREEN-Implementada validación de nombre nulo/vacío en servicio de estudiante

--- src/main/java/com/alkemy/tdd/studentmanagement/service/EstudianteService.java
+++ src/main/java/com/alkemy/tdd/studentmanagement/service/EstudianteService.java
@@ -7,12 +7,12 @@ public class EstudianteService {
     private static Long nextId = 1L;
 
     public Estudiante crearEstudiante(Estudiante estudiante) {
-        //if (estudiante == null) {
-        //    throw new IllegalArgumentException("El estudiante no puede ser nulo.");
-        //}
-        //if (estudiante.getNombre() == null || estudiante.getNombre().trim().isEmpty()) {
-        //    throw new IllegalArgumentException("El nombre del estudiante es requerido.");
-        //}
+        if (estudiante == null) {
+            throw new IllegalArgumentException("El estudiante no puede ser nulo.");
+        }
+        if (estudiante.getNombre() == null || estudiante.getNombre().trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre del estudiante es requerido.");
+        }
 
         estudiante.setId(nextId);
         nextId++;
